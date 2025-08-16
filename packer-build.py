b0dd9a8642a6d5a968cb8fd8d5110046a9e66d1e
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-08-16 07:09:05 -0700

    Check and save latest base AMI

--- packer-build.py
+++ packer-build.py
@@ -6,7 +6,7 @@ import subprocess
 import tempfile
 import boto3
 
-def main():
+def build(codename):
     ssm = boto3.client("ssm")
 
     # 1) Read JSON args from SSM Parameter Store (SecureString)
@@ -24,8 +24,6 @@ def main():
         os.chmod(key_path, stat.S_IRUSR | stat.S_IWUSR)  # 0o600
 
         # 3) Build the packer command
-        #    Pull UBUNTU_CODENAME from env (like your original)
-        ubuntu_codename = os.environ.get("UBUNTU_CODENAME")
         cmd = [
             "packer", "build",
             "-var", f"region={params['region']}",
@@ -33,7 +31,7 @@ def main():
             "-var", f"security_group_id={params['security_group_id']}",
             "-var", f"subnet_id={params['subnet_id']}",
             "-var", f"ssh_private_key_file={key_path}",
-            "-var", f"ubuntu_codename={ubuntu_codename}",
+            "-var", f"ubuntu_codename={codename}",
             ".",
         ]
 
@@ -45,6 +43,39 @@ def main():
             os.remove(key_path)
         except FileNotFoundError:
             pass
+
+def get_last_base(codename):
+    ssm = boto3.client("ssm")
+    resp = ssm.get_parameter(
+        Name=f"/infrahouse/ubuntu-pro/latest/{codename}",
+    )
+    return json.loads(resp["Parameter"]["Value"])
+
+
+def set_last_base(codename, ami_id):
+    ssm = boto3.client("ssm")
+    ssm.put_parameter(
+        Name=f"/infrahouse/ubuntu-pro/latest/{codename}",
+        Value=ami_id,
+        Type="String",
+        Overwrite=True
+    )
+
+def get_latest_ubuntu_ami(codename, product="pro-server"):
+    ssm = boto3.client("ssm")
+    resp = ssm.get_parameter(
+        Name=f"/aws/service/canonical/ubuntu/{product}/{codename}/stable/current/amd64/hvm/ebs-gp3/ami-id",
+    )
+    return json.loads(resp["Parameter"]["Value"])
+
+
+def main():
+    ubuntu_codename = os.environ.get("UBUNTU_CODENAME")
+    latest_ubuntu_ami = get_latest_ubuntu_ami(ubuntu_codename)
+    if get_last_base(ubuntu_codename) != latest_ubuntu_ami:
+        build(ubuntu_codename)
+        set_last_base(ubuntu_codename, latest_ubuntu_ami)
+
 
 if __name__ == "__main__":
     main()
